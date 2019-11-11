d91b93ce297dd1d8e6e7e4cfa495d5e7a978df34
Iain Woodburn <enjkw13@gmail.com>
2019-11-10 20:51:02 -0600

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,81 +1,157 @@
 import pandas as pd
 import numpy as np
 import sys
-import time
-import math
-from scipy.spatial.distance import pdist
 
-
+ 
+columns = ['latitude', 'longitude', 'reviewCount', 'checkins']
+ 
 class Cluster():
     def __init__(self, neighbors, values):
         self.neighbors = neighbors
         self.values = values
-        
-def calc_centroid():
+ 
+def within_cluster_score(clusters, dist_type='euchlidean'):
+    square_err = 0
+    for cluster in clusters:
+        for row, index in cluster.neighbors.iterrows():
+            if dist_type.strip().lower() == 'manhattan':
+                square_err += np.square(np.sum(abs(np.subtract(row, cluster.values))))
+            else:
+                square_err += np.square(np.sum(np.linalg.norm(row - cluster.values)))
+    return square_err
+
+def improved_scoring(clusters):
     pass
-        
-if __name__ == '__main__':
-    
-    train_file = '..\data\given\\test.csv'#sys.argv[1]
-    k_val = 4#sys.argv[2]
-    cluster = 1#sys.argv[3]
-    
-    columns = ['latitude', 'longitude', 'reviewCount', 'checkins']
-    
-    data = pd.read_csv(train_file, delimiter=',', dtype=float, usecols=columns, index_col=None, engine='python')
-    
+ 
+def KNN(k_val, data, iterations, dist_type='euchlidean'):
     #chooses k clusters from the data (randomly)
     k_clusters = data.sample(n = k_val)
     clusters = []
+    terminate = False
     
-    #create a Cluster object for each random cluster
-    for index, row in k_clusters.iterrows():
-        clusters.append(Cluster(pd.DataFrame(columns=columns), row))
+    print('k_clusters: ', k_clusters)
+ 
+    while not terminate:
+ 
+        #create a Cluster object for each random cluster
+        #O(k)
+#        print('k_clusters: ', k_clusters)
+        clusters = []
+        for index, row in k_clusters.iterrows():
+            clusters.append(Cluster(pd.DataFrame(columns=columns), row))
+            
+            
+ 
+#        print('k_clusters: ', k_clusters)
+#        print('difference: ', pd.concat([data, k_clusters, k_clusters]).drop_duplicates(keep=False))
+    
+        #for every index (except clusters), find it's nearest neighbor
+        for index, row in pd.concat([data, k_clusters, k_clusters]).drop_duplicates(keep=False).iterrows():
+#            print('here')
+            closest_cluster = None
+            shortest_dist = np.inf
+ 
+            #test the difference to each cluster and select the closest
+            for cluster in clusters:
+ 
+                if dist_type.strip().lower() == 'manhattan':
+                    dist = np.sum(abs(np.subtract(row, cluster.values)))
+                else:
+                   #calculate L2 (euchlidean) distance as default
+                    dist = np.linalg.norm(row - cluster.values)
+ 
+                if dist <= shortest_dist:
+                    shortest_dist = dist
+                    closest_cluster = cluster
+ 
+            closest_cluster.neighbors = closest_cluster.neighbors.append(row)
+ 
         
-        
-    print(len(data))
-    #for every index, find it's nearest neighbor
-    for index, row, in data.iterrows():
-        closest_cluster = None
-        shortest_dist = test = math.inf
-        
-        #test the difference to each cluster and select the closest
+    
+        #clears the DataFrame, since these will not be the clusters any more
+        compare_clusters = k_clusters.copy()
+        k_clusters = k_clusters.iloc[0:0]
+#        print('k_clusters: ', k_clusters)
+#        print(clusters)
+        #calculate the mean for each column
         for cluster in clusters:
-            
-            #skip if it's the same datapoint
-            if row.equals(cluster.values):
-                continue
-            
-            dist = np.linalg.norm(row - cluster.values)
-            
-            if dist <= shortest_dist:
-                shortest_dist = dist
-                closest_cluster = cluster
+#            print('here')
+#            print(cluster.neighbors)
+            for column in columns:
+                #calculate new centroid coordinates for each of the four columns
+                #include current centroid values into calculation as well
                 
-        closest_cluster.neighbors = closest_cluster.neighbors.append(row)
+                if len(cluster.neighbors) > 0:
+#                    print('before: ', cluster.values[column])
+                    cluster.values[column] = (np.sum(cluster.neighbors[column]) + cluster.values[column]) / len(cluster.neighbors)
+#                    print('after: ', cluster.values[column])
+            k_clusters = k_clusters.append(cluster.values)
+#            print('k_clusters: ', k_clusters)
+#            
+#        print('k_clusters: ', k_clusters)
+#        print('clusters: ', clusters)
         
+#        print('compare_clusters: ', compare_clusters)
+#        print('k_clusters: ', k_clusters)
+#        print()
         
-#    print(clusters[0].neighbors.head())
-#    
-#    print(len(clusters[0].neighbors))
-#    print(len(clusters[1].neighbors))
-#    print(len(clusters[2].neighbors))
-#    print(len(clusters[3].neighbors))
-    
-    
-    for cluster in clusters:
-        print('old: ', cluster.values)
+        #either nothing has changed or the iterations have run out
+        if compare_clusters.equals(k_clusters) or iterations is 0:
+            terminate = True
+#            print(iterations)
+        iterations += 1
+    print(iterations)
+    return clusters
+ 
+ 
+if __name__ == '__main__':
+ 
+    train_file = '..\data\given\\test.csv'#sys.argv[1]
+    k_val = 3#sys.argv[2]
+    cluster = 1#sys.argv[3]
+    iterations = 1
+ 
+    data = pd.read_csv(train_file, delimiter=',', dtype=float, usecols=columns, index_col=None, engine='python')
+#    print(data)
+#    for k_val in [3, 6, 9, 12, 24]:
+#        pass
+ 
+    centroids = None
+ 
+    if cluster is 1:
+        #regular euchlidean distance
+        centroids = KNN(k_val, data, iterations)
+        print('WC-SSE=', within_cluster_score(centroids))
+    elif cluster is 2:
+        #log transform
+        data['reviewCount'] = np.log2(data['reviewCount'])
+        data['checkins'] = np.log2(data['checkins'])
+        centroids = KNN(k_val, data, iterations)
+        print('WC-SSE=', within_cluster_score(centroids))
+    elif cluster is 3:
+        #standardize
+        #x_std = (x - mean) / standard_deviation
         for column in columns:
-            #calculate new centroid coordinates for each of the four columns
-            #include current centroid values into calculation as well
-            cluster.values[column] = (np.sum(cluster.neighbors[column]) + cluster.values[column]) / len(cluster.neighbors)
-        print('new: ', cluster.values)
-        print()
-    
-    
-    #merged should always be an empty dataframe, so that every data point is assigned
-    #to a unique cluster point
-#    merged = pd.merge(clusters[2].neighbors, clusters[3].neighbors, on=columns, how='inner')
-#    print('merged: ', merged)
-    
-    
+            data[column] = (data[column] - data[column].mean()) / data[column].std()
+        centroids = KNN(k_val, data, iterations)
+        print('WC-SSE=', within_cluster_score(centroids))
+    elif cluster is 4:
+        #manhattan distance
+        centroids = KNN(k_val, data, iterations, 'manhattan')
+        print('WC-SSE=', within_cluster_score(centroids, 'manhattan'))
+    elif cluster is 5:
+        #random sample of data
+        sampled_data = data.sample(n = int(np.floor((6 * data.shape[0]) / 100)))
+        centroids = KNN(k_val, data, iterations)
+        print('WC-SSE=', within_cluster_score(centroids))
+    elif cluster is 6:
+        #improved score function
+        centroids = KNN(k_val, data, iterations)
+        print('WC-SSE=', improved_scoring(centroids))
+ 
+    k = 0
+    for centroid in centroids:
+        print('Centroid', k, '=[',
+        centroid.values['latitude'], centroid.values['longitude'],
+        centroid.values['reviewCount'], centroid.values['checkins'], ']')
+        k += 1
