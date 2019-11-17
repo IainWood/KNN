7d987e307f04b843a70a26cf60ac7d3faf28ed44
Iain Woodburn <enjkw13@gmail.com>
2019-11-17 16:44:01 -0600

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -13,24 +13,30 @@ def within_cluster_score(clusters, k_clusters, dist_type='euclidean'):
     return square_err
 
 #minimize the within cluster sum of squares & maximize the distance between centroids
-def improved_scoring(clusters):
+def improved_scoring(clusters, k_clusters, dist_type='euclidean'):
     dist = 0
-    for cluster_i in clusters:
-        for cluster_j in clusters:
-            dist += np.square(np.linalg.norm(cluster_i.values - cluster_j.values))
-    return within_cluster_score(clusters) - dist
+    for index_i, cluster_i in clusters.items():
+        
+        for index_j, cluster_j in clusters.items():
+            if dist_type.strip().lower() == 'manhattan':
+                dist += np.square(np.sum(abs(np.subtract(cluster_i, cluster_j))))
+            else:
+                dist += np.square(np.linalg.norm(np.subtract(cluster_i, cluster_j)))
+    return within_cluster_score(clusters, k_clusters) - dist
 
 def KNN(k_val, data, converge_val = 0.00000001, dist_type='euclidean'):
     
-    clusters = {}
+    #select random indexes from the data
     rands = np.random.choice(len(data), k_val, replace=False)
+    clusters = {}
     k = 0
-    for rand in rands:
+    for rand in rands:      #fill in the clusters with initial data points
         clusters[k] = data[rand]
         k += 1
-            
-    ret_classes = {}
+        
+    ret_clusters = {}
     optimal = False
+    #while the values are not close enough
     while not optimal:
         k_clusters = {}
         
@@ -48,59 +54,63 @@ def KNN(k_val, data, converge_val = 0.00000001, dist_type='euclidean'):
             shortest = dist.index(min(dist))
             k_clusters[shortest].append(row)
             
-        closest_cluster = dict(clusters)
+        compare_cluster = dict(clusters)
 
+        #compute new clusters
         for neighbor in k_clusters:
             clusters[neighbor] = np.average(k_clusters[neighbor], axis = 0)
             
         for cluster in clusters:
-            old = closest_cluster[cluster]
+            old = compare_cluster[cluster]
             new = clusters[cluster]
-
+            
             if np.sum((new - old)/old * 100.0) > converge_val:
                 optimal = True
-        ret_classes = k_clusters
+        ret_clusters = k_clusters
         
-    return clusters, ret_classes
+    return clusters, ret_clusters
             
 if __name__ == "__main__":
     
-    train_file = '..\data\given\\test.csv'#sys.argv[1]
-    k_val = 25#int(sys.argv[2])
-    cluster = 3#int(sys.argv[3])
+    train_file = sys.argv[1]
+    k_val = int(sys.argv[2])
+    cluster_type = int(sys.argv[3])
+
     #treat it as a numpy array, NOT pandas
-    data = pd.read_csv(train_file, delimiter=',', dtype=float, usecols=['latitude', 'longitude', 'reviewCount', 'checkins'], index_col=None, engine='python')
+    data = pd.read_csv(train_file, delimiter=',', 
+                       dtype=float, 
+                       usecols=['latitude', 'longitude', 'reviewCount', 'checkins'], 
+                       index_col=None, 
+                       engine='python')
     log_data = data.copy()
     std_data = data.copy()
     sampled_data = data.sample(n = int(np.floor((6 * data.shape[0]) / 100))).values
     data = data.values
     
-    if cluster is 1: #regular euchlidean distance
+    if cluster_type is 1: #regular euclidean distance
         clusters, k_clusters = KNN(k_val, data)
         print('WC-SSE=', within_cluster_score(clusters, k_clusters))
-    elif cluster is 2: #log transform
-        log_data['reviewCount'] = np.log2(log_data['reviewCount'])
+    elif cluster_type is 2: #log transform
+        log_data['reviewCount'] = np.where(log_data['reviewCount'] != 0, np.log2(log_data['reviewCount']), 0)
         log_data['checkins'] = np.log2(log_data['checkins'])
         clusters, k_clusters = KNN(k_val, log_data.values)
         print('WC-SSE=', within_cluster_score(clusters, k_clusters))
-    elif cluster is 3: #standardize
+    elif cluster_type is 3: #standardize
         for column in std_data.columns:
             std_data[column] = (std_data[column] - std_data[column].mean()) / std_data[column].std()
-        print(std_data)
-        clusters, k_clusters = KNN(k_val, std_data)
+        clusters, k_clusters = KNN(k_val, std_data.values)
         print('WC-SSE=', within_cluster_score(clusters, k_clusters))
-    elif cluster is 4: #manhattan distance
+    elif cluster_type is 4: #manhattan distance
         clusters, k_clusters = KNN(k_val, data)
         print('WC-SSE=', within_cluster_score(clusters, k_clusters, dist_type='manhattan'))
-    elif cluster is 5:  #random sample of data
-        clusters, k_clusters = KNN(k_val, data)
+    elif cluster_type is 5:  #random sample of data
+        clusters, k_clusters = KNN(k_val, sampled_data)
         print('WC-SSE=', within_cluster_score(clusters, k_clusters))
-    elif cluster is 6: #improved score function
+    elif cluster_type is 6: #improved score function
         clusters, k_clusters = KNN(k_val, data)
         print('WC-SSE=', improved_scoring(clusters, k_clusters))
     
     k = 1
-#    clusters, k_clusters = KNN(k_val, data)
     for index, cluster in clusters.items():
         print('Centroid' + str(k) + '=' + str(list(cluster)))
         k += 1
